ffe117a1a70e585a11e98c7b4dde08b207516b30
Keith Lee <keith.lee@gumstix.com>
2017-12-11 11:35:30 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Gumstix RoomSense Christmas IoTree
+
+## Introduction
+This RoomSense sketch is designed to activate a [power relay](http://digitalloggers.com) with passive IR occupancy detection or by remote control via [MyDevices](http://www.mydevices.com)  Cayenne.
+
+## Files
+There are three files in this sketch.  The first is this readme.  Cayenne_Xmas_Tree.ino is the main application and settings.h.example is a template of the manual settings required for 
---
+++ RoomSense_IoTree.ino
@@ -0,0 +1,137 @@
+#define RELAY_PIN 10
+#define UB_LED_PIN 9
+// #define CAYENNE_DEBUG
+#define CAYENNE_PRINT Serial
+#define TX_INTERVAL 300
+#define PIR_DOUT 0
+
+
+#include <CayenneMQTTMKR1000.h>
+#include "settings.h"
+#include <SparkFun_Si7021_Breakout_Library.h>
+#include <Wire.h>
+#include <MS5611.h>
+#include <WiFi101OTA.h>
+
+int idleTime = 0;
+bool wasActive = false;
+bool relayOn = false;
+
+float humidity = 0;
+float temp = 0;
+float pressure = 0;
+int idleMinutes = 30;
+int millisStart;
+int millisInter;
+int millisLastTx;
+Weather sensor;
+MS5611 Psensor(&Wire);
+
+int pirStatus;
+
+void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(UB_LED_PIN, OUTPUT);
+  pinMode(PIR_DOUT, INPUT);
+  digitalWrite(RELAY_PIN, LOW);
+  
+  Serial.begin(9600);
+  Psensor.connect();
+  sensor.begin();
+
+  Cayenne.begin(mqttUser, mqttPass, mqttID, ssid, pass);
+  millisStart = millis();
+  millisLastTx = millisStart;
+
+  WiFiOTA.begin("OfficeTree", otaPass, InternalStorage);
+  Cayenne.virtualWrite(4, idleMinutes, "IdleTime", "Minutes");
+}
+
+void loop() {
+pirStatus = digitalRead(PIR_DOUT);
+  if(pirStatus && !wasActive)
+  {
+    if(!relayOn)
+    {
+      digitalWrite(RELAY_PIN, HIGH);
+      relayOn = true;  
+      idleTime = 0;
+      Serial.print("Relay going active\n");
+    }
+    wasActive = true;
+    
+  }
+  else if(!pirStatus)
+  {
+    if(wasActive)
+    {
+      wasActive = false;
+      millisStart = millis();
+      Serial.print("Occupant has left\n");
+    }
+    else
+    {
+      
+      millisInter = millis();
+      if(millisInter - millisLastTx >= TX_INTERVAL)
+      {
+        txData();
+        // Serial.print("Transmitted to Cayenne\n");
+        millisLastTx = millisInter;
+      }
+      if(millisInter - millisStart > 60000)
+      {
+        idleTime++;
+        millisStart = millisInter;
+        Serial.print("Tick\n");
+      }
+    }
+    if(idleTime >= idleMinutes && relayOn)
+    {
+      digitalWrite(RELAY_PIN, LOW);
+      relayOn = false;
+      Serial.print("Time expired.  Relay off\n");
+    }
+  }
+  WiFiOTA.poll();
+  Cayenne.loop();
+}
+
+void txData()
+{
+  // Measure Relative Humidity from the HTU21D or Si7021
+  humidity = sensor.getRH();
+
+  // Measure Temperature from the HTU21D or Si7021
+  temp = sensor.getTemp();
+  // Temperature is measured every time RH is requested.
+  // It is faster, therefore, to read it from previous RH
+  // measurement with getTemp() instead with readTemp()
+
+  Psensor.ReadProm();
+  Psensor.Readout();
+  pressure = Psensor.GetPres()/100.0;
+  Cayenne.celsiusWrite(1, temp);
+  Cayenne.virtualWrite(2, humidity, "rel_hum","p");
+  Cayenne.virtualWrite(3, pressure, "bp", "pa");
+  Cayenne.virtualWrite(5, relayOn, "prox", "bool");
+  Cayenne.virtualWrite(6, idleTime, "time", "minutes");
+}
+
+CAYENNE_IN(4)
+{
+  idleMinutes = getValue.asInt();
+  Serial.print("Recived new idle timeout\n");
+}
+CAYENNE_IN(7)
+{
+  digitalWrite(RELAY_PIN, HIGH);
+  relayOn = true;
+  idleTime = 0;
+  wasActive = true;
+  delay(50);
+  Cayenne.virtualWrite(7, 0, "reply", "bool");
+  Serial.print("Manually activated the tree");
+}
+
